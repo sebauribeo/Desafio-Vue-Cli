b9a650b0a7716f34461c5d11c72717c990ca209f
sebauribeo <chefsebauribe@gmail.com>
2020-11-24 19:32:35 -0300

    desafio

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,55 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="hello container">
+    <h2 class="text-center m-4cd ">Tabla</h2>
+    <table class="table table-dark">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>Nombre</th>
+      <th>Apellido</th>
+      <th>Dirección</th>
+      <th>Ciudad</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="(user, index) in usuarios" :key="index">
+      <th>{{user.id}}</th>
+      <td>{{user.name}}</td>
+      <td>{{user.lastname}}</td>
+      <td>{{user.address}}</td>
+      <td>{{user.city}}</td>
+    </tr>
+
+
+  </tbody>
+</table>
+    
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  data() {
+    return {
+      usuarios: [
+        {id: 1, name: 'Erik', lastname: 'Klein', address: 'Roosevelt Causeway', city: 'South Olen'},
+        {id: 2, name: 'Cathryn', lastname: 'Heathcote', address: 'Ernser Gateway', city: 'Winstonville'},
+        {id: 3, name: 'Anika', lastname: 'Muller', address: 'Jacobson Center', city: 'Weberhaven'},
+        {id: 4, name: 'Eliezer', lastname: 'Kub', address: 'Abernathy Inlet', city: 'Noblefort'},
+        {id: 5, name: 'Raegan', lastname: 'Bayer', address: 'Rudy Causeway', city: 'Dietrichton'},
+        {id: 6, name: 'Margarita', lastname: 'Cronin', address: 'Deven Island', city: 'East Camden'},
+        {id: 7, name: 'Caleb', lastname: 'Bergstrom', address: 'Maximillian Harbors', city: 'Gerlachbury'},
+        {id: 8, name: 'Samantha', lastname: 'Schinner', address: 'Bechtelar Lane', city: 'Prohaskamouth'},
+        {id: 9, name: 'Anabelle', lastname: 'Cruickshank', address: 'Collins Walks', city: 'Rueckerside'},
+        {id: 10, name: 'Dedric', lastname: 'Abshire', address: 'Schmeler Islands', city: 'Streichfurt'},
+      ]
+
+    }
+  },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style>
+
